dd512bd0a0a23fe9a57cba9a739e98f21362c611
cedrickkouek <cedrickkouek@gmail.com>
2024-06-06 00:13:24 +0100

    Data manipulation task

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,10 @@
 const getListStudentIds = (listStudents) => {
     let listStudentIds = [];
-    if(!(listStudents instanceof Array)) {
-        return listStudentIds;
+    if (!(listStudents instanceof Array)) {
+      return listStudentIds;
     }
     listStudentIds = listStudents.map((student) => student.id);
     return listStudentIds;
-};
-
-export default getListStudentIds;
+  };
+  
+  export default getListStudentIds;
